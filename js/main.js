4f98989616b5776b41fee8a5322d415fb579e82c
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-07 16:25:14 +0600

    simple update

--- js/main.js
+++ js/main.js
@@ -146,3 +146,11 @@ const cleanPreviousInfo = details => {
     const FoodDetails = document.getElementById(details);
     FoodDetails.innerHTML = "";
 };
+
+//For Mobile navBar
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
